ac02919611dff6f5e748e37fac3a97d10b6c59bb
Frityet <ambhogal01@gmail.com>
2021-12-16 10:55:18 -0800

    lol segfaults

--- managed.c
+++ managed.c
@@ -16,33 +16,31 @@
 #include <string.h>
 #include <assert.h>
 
-#include "utilities.h"
 
-
-ATTRIBUTE(always_inline)
-struct managed_pointer *metadataof(void *ptr)
-{
-    return ptr - sizeof(struct managed_pointer);
-}
-
-void default_deconstructor(void *ptr)
+void default_deconstructor(ATTRIBUTE(unused) void *ptr)
 {
     // free(ptr);
 }
 
 void *managed_alloc(size_t size, size_t count, void (*deconstructor)(void *))
 {
+    //Full size pf the allocation
     size_t realsize = size * count;
+
+    //The realsize is the what the user will use
+    //We also need to allocation size for the metadata
     struct managed_pointer *mptr = malloc(realsize + sizeof(struct managed_pointer));
     mptr->size = realsize;
     mptr->count = count;
+    mptr->type_size = size;
 
     if (mptr->deconstructor == NULL) {
         mptr->deconstructor = default_deconstructor;
     } else {
-        mptr->deconstructor = free;
+        mptr->deconstructor = deconstructor;
     }
 
+    //Just C things
     mptr->data = mptr + 1;
     return mptr->data;
 }
@@ -50,42 +48,49 @@ void *managed_alloc(size_t size, size_t count, void (*deconstructor)(void *))
 void *resize_managed_alloc(void *alloc, size_t size)
 {
     //First, get the metadata inside the allocation
-    struct managed_pointer *mdata = metadataof(alloc);
+    managed struct managed_pointer *mdata = metadataof(alloc);
     assert(mdata->data == alloc);
 
     //Then add on the new size, we will use the old size when we copy the memory
-    int oldsize = mdata->size,
-        newsize = oldsize + size;
+    size_t  oldsize = mdata->size,
+            newsize = oldsize + size;
 
     //Allocate the new memory and copy the old data
     void *newptr = managed_alloc(newsize, 1, mdata->deconstructor);
-    memcpy(newptr, alloc, oldsize);
+    memcpy(metadataof(newptr)->data, mdata->data, oldsize);
 
-    //Free the old allocation
-    mdata->deconstructor(alloc);
-    free(mdata);
     return newptr;
 }
 
-void *resize_managed_array(void *array, int count)
+void *increase_managed_array(void *array, int count)
 {
-    int oldcount = metadataof(array)->count;
-    void *newalloc = resize_managed_alloc(array, sizeof(array[0]) * count); //fuck your memory safety
-    struct managed_pointer *mdata = metadataof(newalloc);
-    mdata->count += count + oldcount; 
-    return newalloc;
+    //Get the metadata of the old array, and the metadata of a newly allocated array
+    struct managed_pointer  *old_array = metadataof(array),
+                            *new_array = metadataof(managed_alloc(old_array->type_size,
+                                                                  old_array->count + count,
+                                                                  old_array->deconstructor));
+
+    //Copy the data and add the count
+    new_array->count += count;
+    new_array->data = memcpy(new_array->data, old_array->data, old_array->size);
+
+    //This sometimes segfaults, but it's a memory leak otherwise
+    free_managed(old_array->data);
+    return new_array->data;
 }
 
 void free_managed(void *ptr)
 {
+    //The cleanup attribute returns a pointer to the variable,
+    //therefore it's actually a pointer to a pointer, so we must dereference
     void *realptr = *(void **)ptr;
     struct managed_pointer *mdata = metadataof(realptr);
 
-    if (mdata->data != ptr) {
-        fprintf(stderr, "Error: metadata pointer (%p) does not equal given pointer (%p)! Are you sure this a managed pointer (allocated with alloc_managed)?\n", mdata->data, ptr);
+    if (mdata->data != realptr) {
+        //TODO: This is probably a bad idea. Too bad!
+        fprintf(stderr, "Error: metadata pointer (%p) does not equal given pointer (%p)! Are you sure this a managed pointer (allocated with alloc_managed)?\n", mdata->data, realptr);
+        exit(EXIT_FAILURE);
     }
-
-    // assert(mdata->data == ptr);
     
     mdata->deconstructor(ptr);
     free(mdata);
@@ -93,8 +98,18 @@ void free_managed(void *ptr)
 
 char *mstr(const char *string)
 {
+    //strlen 🤮🤮🤮
     size_t len = strlen(string);
     char *str = new(char, len);
     strncpy(str, string, len);
     return str;
+}
+
+char *mstrcpy(char *str)
+{
+    //This is literally the only difference between this and mstr, but it's faster because we don't call strlen
+    size_t len = lengthof(str);
+    char *newstr = new(char, len);
+    strncpy(newstr, str, len);
+    return newstr;
 }
