42e4376c36826f6cb757b03ff90ad65834962c9d
Amrit Bhogal <ambhogal01@gmail.com>
2022-05-01 00:06:41 -0700

    Update package.lua

--- package.lua
+++ package.lua
@@ -5,6 +5,7 @@ package("managedc")
     add_urls("https://github.com/Frityet/ManagedC.git")
     add_versions("1.0.0", "350800021f8d41e5717e76f8dfb0485d45781996")
     add_versions("1.1.0", "878158a7f185aaaa38b8855afd91f89b05c9df13")
+    add_versions("1.1.1", "eb70de26ca4cc724efd14dc50ec9bc990ec92271")
 
     on_install(function (package)
         local configs = {}
