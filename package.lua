569b41da630532ddf68729726addd85e59f7f0c7
Amrit Bhogal <ambhogal01@gmail.com>
2022-05-01 15:47:56 -0700

    Update package.lua

--- package.lua
+++ package.lua
@@ -6,6 +6,7 @@ package("managedc")
     add_versions("1.0.0", "350800021f8d41e5717e76f8dfb0485d45781996")
     add_versions("1.1.0", "878158a7f185aaaa38b8855afd91f89b05c9df13")
     add_versions("1.1.1", "eb70de26ca4cc724efd14dc50ec9bc990ec92271")
+    add_versions("1.1.2", "0068462307a1901a1b30410ae53721ca7e88cb10")
 
     on_install(function (package)
         local configs = {}
@@ -15,5 +16,6 @@ package("managedc")
 
     on_test(function (package)
          assert(package:has_cfuncs("mc_alloc_managed", { includes = "managed.h" }))
+         assert(package:has_cfuncs("managed_string", { includes = "mstring.h" }))
     end)
 package_end()
