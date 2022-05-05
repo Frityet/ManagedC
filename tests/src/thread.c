aa226e107fc7722903badd281fedbd9d2f2522a5
Frityet <ambhogal01@gmail.com>
2022-05-05 09:39:23 -0700

    Redoing thread test

--- tests/src/thread.c
+++ tests/src/thread.c
@@ -7,44 +7,9 @@
 
 #include <pthread.h>
 
-void *thread_func(int **ref)
-{
-    //You can get a the reference from the main or this thread
-    auto int *ptr = mc_reference(*ref);
-    ptr = mc_realloc_managed(ptr, 8);
-    if (ptr == NULL) {
-//    TEST_EXPR(ptr != NULL, "Could not reallocate reference!");
-        fprintf(stderr, "Could not reallocate reference!\n");
-    }
-    for (int i = 0; i < 8; i++) {
-        ptr[i] = 8 - i;
-    }
 
-    *ref = ptr;
-    return ptr;
-}
-
-bool resize_ref(int **ref)
-{
-    auto int *ptr = mc_reference(*ref);
-    ptr = mc_realloc_managed(ptr, 16);
-    TEST_EXPR(ptr != NULL, "Could not reallocate reference!");
-
-    *ref = ptr;
-    return true;
-}
-
+//TODO: Make a proper threading test
 TEST(thread)
 {
-    auto int *ptr = mc_alloc_managed(sizeof(int), 1, NULL);
-    pthread_t thr;
-    pthread_create(&thr, NULL, (void *)thread_func, &ptr);
-    resize_ref(&ptr);
-
-    LOG_DEBUG("Ptr count: %d", mc_countof(ptr));
-    pthread_join(thr, NULL);
-
-    LOG_DEBUG("Ptr count: %d", mc_countof(ptr));
-
     return true;
 }
