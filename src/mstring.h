d5e445d4d1aed726226342b15704cb64ffa667c6
Amrit Bhogal <ambhogal01@gmail.com>
2022-05-20 23:47:54 -0700

    str

--- src/mstring.h
+++ src/mstring.h
@@ -20,7 +20,7 @@
 #endif
 
 #if !defined(MC_NO_MSTR_MACRO)
-//#   define mstr(str, ...) managed_string(str, __VA_OPT__(!)true ? strlen(str) : __VA_OPT__(1) __VA_ARGS__)
+#   define mstr(str) managed_string(str, strlen(str))
 #endif
 
 /**
@@ -29,6 +29,7 @@
  * @param len Length of the string.
  * @return Managed pointer to the new string.
  */
+ATTRIBUTE(used)
 static inline string MC_ADD_PREFIX(managed_string)(const string str, int len)
 {
     //+1 for the null term
@@ -48,6 +49,7 @@ static inline string MC_ADD_PREFIX(managed_string)(const string str, int len)
  * @remarks This function does not function as base @c strncat. Instead of writing through the buffer of @c dst, this function
  * allocates a new object, and returns it.
  */
+ATTRIBUTE(used)
 static inline string MC_ADD_PREFIX(mstrcat)(string dst, string src, int len)
 {
     int oldlen = MC_ADD_PREFIX(countof)(dst);
@@ -69,6 +71,7 @@ static inline string MC_ADD_PREFIX(mstrcat)(string dst, string src, int len)
  * @param len
  * @return
  */
+ATTRIBUTE(used)
 static inline string MC_ADD_PREFIX(mstrcpy)(string dst, string src, int len)
 {
     string new = MC_ADD_PREFIX(realloc_managed)(dst, len);
