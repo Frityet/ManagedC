42116322fd111198a9b80f8d9a234d4b55337451
Amrit Bhogal <ambhogal01@gmail.com>
2022-08-20 16:56:25 -0700

    Update package.lua

--- package.lua
+++ package.lua
@@ -17,7 +17,7 @@ package("managedc")
     add_versions("1.5.0", "f4cce9c1aee952d603c18b73dc6219ea15b91717")
     add_versions("2.0.0-beta", "6963abbe67fccce834e4cc1c5d3ecdb73a55701f")
     add_versions("2.1.0-beta", "1217d7f55ee04f2b692e998e451b5a6b000f4618")
-    add_versions("2.2.0-beta", "5a5f7a7717407d23c92cfc6ecdc3462a79831d40")
+    add_versions("2.2.0-beta", "ca8f6af418e3d4713cb902128232c15e2dafb071")
 
     on_install(function (package)
         os.cp("src/managed.h", package:installdir("include"))
